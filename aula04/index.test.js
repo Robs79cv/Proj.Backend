ae486a7da612868f3d329f4f3f86bb7ff0ec709f
Robson Carneiro <robsontec.viana79@gmail.com>
2024-03-14 21:30:38 -0300

    Organização das pastas

---
+++ aula04/index.test.js
@@ -0,0 +1,11 @@
+const supertest = require( 'supertest' );
+
+const app = require('./index')
+
+const request = supertest(app);
+
+test("Deve retornar 200 no GET", async function(){
+    const response = await request.get("/");
+    console.log(response.status)
+    expect(response.status).toBe(200);
+});
